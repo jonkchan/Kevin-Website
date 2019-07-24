eed6a9d287214876e0560f0c3bf485766965e8f0
junhaohuang1 <junhao.huang524@gmail.com>
2019-07-24 15:52:33 -0400

    added current rank

---
+++ src/components/progress.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import axios from 'axios'
+
+const apiKey = process.env.REACT_APP_LEAGUE_API_KEY
+
+class Progress extends Component {
+  state = {
+    rank:"",
+    lp:"",
+    tier:""
+  };
+
+  componentDidMount(){
+    axios.get("/api/getprogress",
+    {
+      headers:{
+        apiKey:apiKey
+      }
+    }).then(res =>{
+      console.log(res)
+      this.setState({
+        rank:res.data.rank,
+        lp:res.data.lp,
+        tier:res.data.tier
+      })
+    })
+  }
+
+
+  render() {
+    return(
+      <div>
+        <div>Rank: {this.state.tier} {this.state.rank} {this.state.lp} LP</div>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default Progress;
